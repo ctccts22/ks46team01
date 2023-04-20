0c9190133c9fb6d0fedac98af7532d8368cb3236
rockqs <dhwlsdn99@gmail.com>
2023-04-20 13:50:53 +0900

    오진우 2023-04-20

---
+++ src/main/java/ks46team01/mushroom/mushroomGrowth/service/FarmMushroomGrowthService.java
@@ -0,0 +1,47 @@
+package ks46team01.mushroom.mushroomGrowth.service;
+
+import ks46team01.mushroom.mushroomGrowth.dto.FarmMushroomGrowth;
+import ks46team01.mushroom.mushroomGrowth.mapper.FarmMushroomGrowthMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class FarmMushroomGrowthService {
+    private final FarmMushroomGrowthMapper farmMushroomGrowthMapper;
+
+    public FarmMushroomGrowthService(FarmMushroomGrowthMapper farmMushroomGrowthMapper){
+        this.farmMushroomGrowthMapper = farmMushroomGrowthMapper;
+    }
+
+    public List<FarmMushroomGrowth> getFarmMushroomGrowth(String searchKey , String searchValue){
+        if(searchKey != null){
+            switch (searchKey) {
+                case "mushroomGrowthIdx" -> searchKey = "mushroom_growth_idx";
+                case "farmDataIdx" -> searchKey = "farm_data_idx";
+                default -> searchKey = "crop_idx";
+            }
+        }
+
+        System.out.println(searchKey+ "입력값");
+
+
+        System.out.println(searchValue + "찾는값");
+
+
+        List<FarmMushroomGrowth> farmMushroomGrowth = farmMushroomGrowthMapper.getMushroomGrowthInfoById(searchKey, searchValue);
+        return farmMushroomGrowth;
+
+    }
+
+
+
+/*    public List<FarmMushroomGrowth> getFarmMushroomGrowth(){
+        List<FarmMushroomGrowth> farmMushroomGrowth = farmMushroomGrowthMapper.getFarmMushroomGrowth();
+        return farmMushroomGrowth;
+    }*/
+
+
+}
