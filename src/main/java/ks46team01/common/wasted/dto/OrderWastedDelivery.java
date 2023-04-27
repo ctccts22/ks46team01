9e640c6ad2c98d95f466f96813a3ea29345e9d41
ctccts22 <ctccts22@gmail.com>
2023-04-27 10:26:44 +0900

    현준 코드 수정21

---
+++ src/main/java/ks46team01/common/wasted/dto/OrderWastedDelivery.java
@@ -0,0 +1,14 @@
+package ks46team01.common.wasted.dto;
+
+import lombok.Data;
+
+@Data
+public class OrderWastedDelivery {
+    private Long orderWastedDeliveryIdx;
+    private Long orderWastedIdx;
+    private String userName;
+    private Long companyInfoIdx;
+    private String orderWastedDeliveryStatus;
+    private String orderWastedDeliveryCompany;
+    private String orderWastedDeliveryComplete;
+}
