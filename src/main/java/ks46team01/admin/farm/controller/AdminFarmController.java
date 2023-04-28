3137e5e51d109ae124ffa73723a423957bd0e27e
kang11033 <hyeonjun2723@hanmail.net>
2023-04-28 17:06:14 +0900

    2023-04-28 오후 5:06

--- src/main/java/ks46team01/admin/farm/controller/AdminFarmController.java
+++ src/main/java/ks46team01/admin/farm/controller/AdminFarmController.java
@@ -11,43 +11,43 @@ public class AdminFarmController {
     // 커피배지 신청관리 페이지
     @GetMapping("/listOrderFarmAdmin")
     public String adminFarmOrderList() {
-        return "/admin/farm/listOrderFarmAdmin";
+        return "admin/farm/listOrderFarmAdmin";
     }
 
     // 커피배지 신청확인 관리
     @GetMapping("/listConfirmFarmAdmin")
     public String adminFarmConfirmList() {
-        return "/admin/farm/listConfirmFarmAdmin";
+        return "admin/farm/listConfirmFarmAdmin";
     }
 
     // 커피배지 배송관리
     @GetMapping("/listDeliveryFarmAdmin")
     public String adminFarmDeliveryList() {
-        return "/admin/farm/listDeliveryFarmAdmin";
+        return "admin/farm/listDeliveryFarmAdmin";
     }
 
     //폐배지 수거신청 관리
     @GetMapping("/listRequestPickupFarmAdmin")
     public String adminFarmPickupRequestList() {
-        return "/admin/farm/listRequestPickupFarmAdmin";
+        return "admin/farm/listRequestPickupFarmAdmin";
     }
 
     // 폐배지 수거신청확인 관리
     @GetMapping("/listConfirmPickupFarmAdmin")
     public String adminFarmPickupConfirmList() {
-        return "/admin/farm/listConfirmPickupFarmAdmin";
+        return "admin/farm/listConfirmPickupFarmAdmin";
     }
 
 
     // 폐배지 수거배송 관리
     @GetMapping("/listDeliveryPickupFarmAdmin")
     public String adminFarmPickupDeliveryList() {
-        return "/admin/farm/listDeliveryPickupFarmAdmin";
+        return "admin/farm/listDeliveryPickupFarmAdmin";
     }
 
     // 작물 고유코드
     @GetMapping("/listCodeCrop")
     public String cropCodeList() {
-        return "/admin/farm/listCodeCrop";
+        return "admin/farm/listCodeCrop";
     }
 }
