1be6c4c22fc5fcfa6cb3667d24e3d9df74750a18
kang11033 <hyeonjun2723@hanmail.net>
2023-05-08 10:23:29 +0900

    2023-05-04 오후 5:05

---
+++ src/main/java/ks46team01/user/wasted/mapper/UserWastedMapper.java
@@ -0,0 +1,7 @@
+package ks46team01.user.wasted.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserWastedMapper {
+}
---
+++ src/main/java/ks46team01/user/wasted/service/UserWastedService.java
@@ -0,0 +1,4 @@
+package ks46team01.user.wasted.service;
+
+public interface UserWastedService {
+}
