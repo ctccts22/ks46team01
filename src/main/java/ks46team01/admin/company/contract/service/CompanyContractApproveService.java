9ca8bd8a1db807f235fe0c5cdf82924af6e5fe1a
ctccts22 <ctccts22@gmail.com>
2023-05-09 19:01:24 +0900

    1. CompanyContract 완료. 기존 모달창 -> sweetalert로 전환 2. Inventory 기능 시작

--- src/main/java/ks46team01/admin/company/contract/service/CompanyContractApproveService.java
+++ src/main/java/ks46team01/admin/company/contract/service/CompanyContractApproveService.java
@@ -1,17 +1,59 @@
 package ks46team01.admin.company.contract.service;
 
+import ks46team01.admin.info.entity.Admin;
+import ks46team01.common.company.contract.entity.CompanyContract;
+import ks46team01.common.company.contract.entity.CompanyContractApprove;
+import ks46team01.common.company.contract.repository.CompanyContractApproveRepository;
 import ks46team01.common.company.contract.repository.CompanyContractRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 @Transactional
 @AllArgsConstructor
 public class CompanyContractApproveService {
     private final CompanyContractRepository companyContractRepository;
+    private final CompanyContractApproveRepository companyContractApproveRepository;
 
+    public CompanyContractApprove updateCompanyContractApprove(Long companyContractApproveIdx, LocalDate companyContractApproveDate, String companyContractApproveStatus, String companyContractApproveContent, Admin admin, Long companyContractIdx) {
+        CompanyContractApprove exCompanyContractApprove;
+
+        if (companyContractApproveIdx != null) {
+            Optional<CompanyContractApprove> exCompanyContractApproveOptional = companyContractApproveRepository.findWithCompanyContractAndAdminByUsername(companyContractApproveIdx);
+
+            if (exCompanyContractApproveOptional.isPresent()) {
+                exCompanyContractApprove = exCompanyContractApproveOptional.get();
+            } else {
+                exCompanyContractApprove = new CompanyContractApprove();
+                exCompanyContractApprove.setCompanyContractApproveIdx(companyContractApproveIdx);
+            }
+        } else {
+            exCompanyContractApprove = new CompanyContractApprove();
+        }
+
+        Optional<CompanyContract> companyContractOptional = companyContractRepository.findByCompanyContractIdx(companyContractIdx);
+        if (companyContractOptional.isPresent()) {
+            CompanyContract companyContract = companyContractOptional.get();
+
+            // CompanyContractApprove and CompanyContract 사이 관계 설정
+            exCompanyContractApprove.setCompanyContractIdx(companyContract);
+        } else {
+            throw new RuntimeException("CompanyContract not found with companyContractIdx: " + companyContractIdx);
+        }
+
+        exCompanyContractApprove.setCompanyContractApproveDate(Date.valueOf(companyContractApproveDate));
+        exCompanyContractApprove.setCompanyContractApproveStatus(companyContractApproveStatus);
+        exCompanyContractApprove.setCompanyContractApproveContent(companyContractApproveContent);
+        exCompanyContractApprove.setAdminUsername(admin);
+
+        return companyContractApproveRepository.save(exCompanyContractApprove);
     }
+}
 
 
 
--- src/main/java/ks46team01/admin/inventory/controller/InventoryController.java
+++ src/main/java/ks46team01/admin/inventory/controller/InventoryController.java
@@ -1,16 +1,24 @@
 package ks46team01.admin.inventory.controller;
 
 
+import jakarta.servlet.http.HttpSession;
+import ks46team01.admin.info.entity.Admin;
+import ks46team01.admin.inventory.dto.InventoryDTO;
 import ks46team01.admin.inventory.entity.Inventory;
 import ks46team01.admin.inventory.repository.InventoryRepository;
+import ks46team01.admin.inventory.service.InventoryService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @Slf4j
@@ -19,13 +27,48 @@ import java.util.List;
 public class InventoryController {
 
     private final InventoryRepository inventoryRepository;
+    private final InventoryService inventoryService;
 
     @GetMapping("/listInventory")
     public String inventoryList(Model model) {
         List<Inventory> inventoryList = inventoryRepository.findAll();
         model.addAttribute("title", "재고물품관리");
-        model.addAttribute("inventoryList" , inventoryList);
+        model.addAttribute("inventoryList", inventoryList);
         log.info("inventoryList={}", inventoryList);
         return "admin/inventory/listInventory";
     }
+
+    @PostMapping("/addInventory")
+    @ResponseBody
+    public ResponseEntity<?> addInventory(@RequestParam(name = "inventoryIdx", required = false) Long inventoryIdx,
+                                          @RequestParam(name = "inventoryType", required = false) String inventoryType,
+                                          @RequestParam(value = "inventoryDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime inventoryDate,
+                                          HttpSession session) {
+
+        Admin admin = (Admin) session.getAttribute("adminUser");
+        if (admin != null) {
+            InventoryDTO addedInventoryDTO = inventoryService.addCompanyContract(inventoryIdx, inventoryType, inventoryDate, admin);
+            if (addedInventoryDTO == null) {
+                return new ResponseEntity<>("400에러", HttpStatus.BAD_REQUEST);
+            } else {
+                return new ResponseEntity<>("200성공", HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>("401어드민로그인에러", HttpStatus.UNAUTHORIZED);
+    }
+    @PostMapping("/deleteInventory")
+    @ResponseBody
+    public ResponseEntity<?> deleteInventory(@RequestParam("inventoryIdx") Long inventoryIdx, HttpSession session) {
+        Admin admin = (Admin) session.getAttribute("adminUser");
+        if (admin != null) {
+            boolean isDeleted = inventoryService.deleteInventory(inventoryIdx, admin);
+            if (isDeleted) {
+                return new ResponseEntity<>("200성공", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("400에러", HttpStatus.BAD_REQUEST);
+            }
+        }
+        return new ResponseEntity<>("401어드민로그인에러", HttpStatus.UNAUTHORIZED);
+    }
+
 }
