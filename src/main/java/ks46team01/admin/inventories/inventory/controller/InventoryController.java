fdd8bf51fa1ff4b0f261c81d0e5d4a5b65774fa5
ctccts22 <ctccts22@gmail.com>
2023-05-12 00:23:57 +0900

    Inventory function

--- src/main/java/ks46team01/admin/inventories/inventory/controller/InventoryController.java
+++ src/main/java/ks46team01/admin/inventories/inventory/controller/InventoryController.java
@@ -3,10 +3,16 @@ package ks46team01.admin.inventories.inventory.controller;
 
 import jakarta.servlet.http.HttpSession;
 import ks46team01.admin.info.entity.Admin;
+import ks46team01.admin.inventories.input.entity.InventoryInput;
+import ks46team01.admin.inventories.input.repository.InventoryInputRepository;
 import ks46team01.admin.inventories.inventory.dto.InventoryDTO;
 import ks46team01.admin.inventories.inventory.entity.Inventory;
 import ks46team01.admin.inventories.inventory.repository.InventoryRepository;
 import ks46team01.admin.inventories.inventory.service.InventoryService;
+import ks46team01.admin.inventories.output.entity.InventoryOutput;
+import ks46team01.admin.inventories.output.repository.InventoryOutputRepository;
+import ks46team01.admin.inventories.record.entity.InventoryRecord;
+import ks46team01.admin.inventories.record.repository.InventoryRecordRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -24,6 +30,9 @@ import java.util.List;
 @AllArgsConstructor
 @RequestMapping("/admin/inventory")
 public class InventoryController {
+    private final InventoryOutputRepository inventoryOutputRepository;
+    private final InventoryInputRepository inventoryInputRepository;
+    private final InventoryRecordRepository inventoryRecordRepository;
 
     private final InventoryRepository inventoryRepository;
     private final InventoryService inventoryService;
@@ -31,10 +40,35 @@ public class InventoryController {
     @GetMapping("/listInventory")
     public String inventoryList(Model model) {
         List<Inventory> inventoryList = inventoryRepository.findAll();
-        model.addAttribute("title", "재고물품관리");
+        model.addAttribute("title", "재고관리");
         model.addAttribute("inventoryList", inventoryList);
         log.info("inventoryList={}", inventoryList);
         return "admin/inventory/listInventory";
+    }
+
+    @GetMapping("/listInventoryDetail")
+    public String inventoryListDetail(Model model) {
+        List<InventoryRecord> inventoryRecordList = inventoryRecordRepository.findAll();
+        model.addAttribute("title", "재고기록관리");
+        model.addAttribute("inventoryRecordList", inventoryRecordList);
+        log.info("inventoryRecordList={}", inventoryRecordList);
+        return "admin/inventory/listInventoryDetail";
+    }
+    @GetMapping("/listInventoryInput")
+    public String inventoryListInput(Model model) {
+        List<InventoryInput> inventoryInputList = inventoryInputRepository.findAll();
+        model.addAttribute("title", "재고입고");
+        model.addAttribute("inventoryInputList", inventoryInputList);
+        log.info("inventoryInputList={}", inventoryInputList);
+        return "admin/inventory/listInventoryInput";
+    }
+    @GetMapping("/listInventoryOutput")
+    public String inventoryListOutput(Model model) {
+        List<InventoryOutput> inventoryOutputList = inventoryOutputRepository.findAll();
+        model.addAttribute("title", "재고출고");
+        model.addAttribute("inventoryOutputList", inventoryOutputList);
+        log.info("inventoryOutputList={}", inventoryOutputList);
+        return "admin/inventory/listInventoryOutput";
     }
 
     @PostMapping("/addInventory")
@@ -55,6 +89,7 @@ public class InventoryController {
         }
         return new ResponseEntity<>("401어드민로그인에러", HttpStatus.UNAUTHORIZED);
     }
+
     @PostMapping("/deleteInventory")
     @ResponseBody
     public ResponseEntity<?> deleteInventory(@RequestParam("inventoryIdx") Long inventoryIdx, HttpSession session) {
--- src/main/java/ks46team01/admin/inventories/inventory/service/InventoryService.java
+++ src/main/java/ks46team01/admin/inventories/inventory/service/InventoryService.java
@@ -2,21 +2,34 @@ package ks46team01.admin.inventories.inventory.service;
 
 import ks46team01.admin.info.entity.Admin;
 import ks46team01.admin.info.repository.AdminRepository;
+import ks46team01.admin.inventories.input.entity.InventoryInput;
+import ks46team01.admin.inventories.input.repository.InventoryInputRepository;
 import ks46team01.admin.inventories.inventory.dto.InventoryDTO;
 import ks46team01.admin.inventories.inventory.entity.Inventory;
 import ks46team01.admin.inventories.inventory.repository.InventoryRepository;
+import ks46team01.admin.inventories.output.entity.InventoryOutput;
+import ks46team01.admin.inventories.output.repository.InventoryOutputRepository;
+import ks46team01.admin.inventories.record.entity.InventoryRecord;
+import ks46team01.admin.inventories.record.repository.InventoryRecordRepository;
+import ks46team01.common.company.contract.entity.CompanyContract;
+import ks46team01.common.company.contract.entity.CompanyContractApprove;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Date;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Transactional
 @AllArgsConstructor
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
+    private final InventoryInputRepository inventoryInputRepository;
+    private final InventoryOutputRepository inventoryOutputRepository;
+    private final InventoryRecordRepository inventoryRecordRepository;
     private final AdminRepository adminRepository;
 
     public InventoryDTO addCompanyContract(Long inventoryIdx, String inventoryType, LocalDateTime inventoryDate, Admin admin) {
@@ -46,4 +59,5 @@ public class InventoryService {
         }
         return false;
     }
+
 }
