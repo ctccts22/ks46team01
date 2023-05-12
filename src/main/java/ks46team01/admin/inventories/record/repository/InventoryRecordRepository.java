0f2df0443d27f68d0c364558fbbb72dd32e46391
ctccts22 <ctccts22@gmail.com>
2023-05-12 14:21:11 +0900

    Inventory function

--- src/main/java/ks46team01/admin/inventories/record/repository/InventoryRecordRepository.java
+++ src/main/java/ks46team01/admin/inventories/record/repository/InventoryRecordRepository.java
@@ -6,9 +6,13 @@ import ks46team01.common.company.contract.entity.CompanyContract;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface InventoryRecordRepository extends JpaRepository<InventoryRecord, Long> {
 
     InventoryRecord findByInventoryIdx(Inventory inventoryIdx);
 
+    List<InventoryRecord> findByCompanyContractIdx(CompanyContract companyContractIdx);
 }
